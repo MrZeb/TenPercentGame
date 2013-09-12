dbbc6792aaa93318ec3ae5e3313692c1d6ad5bd3
Sebastian <sebastian@footballaddicts.com>
2013-09-12 09:15:11 +0200

    Stuff

--- src/se/footballaddicts/tenpercentgame/MainActivity.java
+++ src/se/footballaddicts/tenpercentgame/MainActivity.java
@@ -1,22 +1,30 @@
 package se.footballaddicts.tenpercentgame;
 
-import android.os.Bundle;
+import se.footballaddicts.tenpercentgame.opengl.OpenGLES20Complete;
 import android.app.Activity;
+import android.content.Intent;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity
+{
+    private GLSurfaceView mGLView;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
+    @Override
+    protected void onCreate( Bundle savedInstanceState )
+    {
+        super.onCreate( savedInstanceState );
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+        startActivity( new Intent( this, OpenGLES20Complete.class ) );
+        finish();
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu( Menu menu )
+    {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate( R.menu.main, menu );
+        return true;
+    }
 }
